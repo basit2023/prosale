da3d4c699fc9d5ac15bb83ad309df482ba2a7b4a
basit2023 <babaralibj362@gmail.com>
2024-06-05 12:59:38 +0500

    encryped part updated

--- src/components/encriptdycriptdata.tsx
+++ src/components/encriptdycriptdata.tsx
@@ -1,15 +1,55 @@
 import { AES, enc } from 'crypto-js';
 
-// Function to encrypt data
-export function encryptData(data:any) {
-    const secretKey="prosalewebsite"
-    const encryptedData = AES.encrypt(JSON.stringify(data), secretKey).toString();
+// Function to encrypt data with salt
+export function encryptData(data: any) {
+ 
+    const secretKey = "prosalewebsite";
+    const salt = "randomsalt123";
+    const saltedData = salt + JSON.stringify(data); // Combine salt with data
+    const encryptedData = AES.encrypt(saltedData, secretKey).toString();
     return encryptedData;
 }
 
-// Function to decrypt data
-export function decryptData(encryptedData:any) {
-    const secretKey="prosalewebsite"
+// Function to decrypt data with salt
+export function decryptData(encryptedData: any) {
+    const secretKey = "prosalewebsite";
+    const salt = "randomsalt123";
+    if(!encryptedData){
+        return;
+    }
     const decryptedData = AES.decrypt(encryptedData, secretKey).toString(enc.Utf8);
-    return JSON.parse(decryptedData);
+   
+   
+    
+    const dataWithoutSalt = decryptedData.replace(salt, '');
+    
+    return JSON.parse(dataWithoutSalt);
 }
+
+
+
+
+
+// import { AES, enc } from 'crypto-js';
+
+// // Function to encrypt data
+// export function encryptData(data:any) {
+//     const secretKey="prosalewebsite"
+//     const encryptedData = AES.encrypt(JSON.stringify(data), secretKey).toString();
+//     return encryptedData;
+// }
+
+// // Function to decrypt data
+// export function decryptData(encryptedData:any) {
+//     const secretKey = "prosalewebsite";
+//     const decryptedBytes = AES.decrypt(encryptedData, secretKey);
+    
+//     if (decryptedBytes && decryptedBytes.toString()) {
+//         const decryptedData = decryptedBytes.toString(enc.Utf8);
+//         return JSON.parse(decryptedData);
+//     } else {
+       
+//         console.error("Decryption failed or returned empty data.");
+//         return null; 
+//     }
+// }
