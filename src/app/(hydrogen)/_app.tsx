1c81762b6650e52afec89ae80b9f244a93890a92
basit2023 <babaralibj362@gmail.com>
2024-06-27 18:10:53 +0500

    code updated

---
+++ src/app/(hydrogen)/_app.tsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js').then(registration => {
+          console.log('SW registered: ', registration);
+        }).catch(registrationError => {
+          console.log('SW registration failed: ', registrationError);
+        });
+      });
+    }
+  }, []);
+
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
