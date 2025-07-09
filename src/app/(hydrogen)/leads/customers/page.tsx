262f0d85477c0d0d6020d75847cd7ff78bdc9676
Ehtesham Ali <ehtesham@elaan.net>
2025-07-09 15:53:56 +0500

    code updated

--- src/app/(hydrogen)/leads/customers/page.tsx
+++ src/app/(hydrogen)/leads/customers/page.tsx
@@ -1,23 +1,47 @@
 'use client';
 import { routes } from '@/config/routes';
-// import { invoiceData } from '@/data/invoice-data';
 import InvoiceTable from '@/components/LeadManagement/AllCustomers/customers/table';
-// import InvoiceTable from '@/components/LeadManagement/employee-data/Highly_Interested/table';
 import { useEmployeeData } from '@/components/LeadManagement/AllCustomers/AllCustomer';
 import TableLayout from './tabel-layout';
-// import TableLayout from '../table-layout';
-import { metaObject } from '@/config/site.config';
 import { useSession } from 'next-auth/react';
 import { Empty } from 'rizzui';
-
+import { useEffect, useState } from 'react';
+import Spinner from '@/components/ui/spinner';
 const pageHeader = {
   title: 'All Customers',
   breadcrumb: [],
 };
 
 export default function EnhancedTablePage() {
-  const { data: session } = useSession();
-  const invoiceData: [] = useEmployeeData(session?.user?.email);
+  const { data: session, status } = useSession();
+  const invoiceData = useEmployeeData(session?.user?.email);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (status === 'loading') return; // Wait for session to load
+    // Wait until data is fetched
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 500); // optional slight delay for UX
+
+    return () => clearTimeout(timeout);
+  }, [status, invoiceData]);
+
+  if (loading || status === 'loading') {
+    return (
+      <div className="flex justify-center items-center h-screen text-lg">
+        <Spinner/>
+      </div>
+    );
+  }
+
+  if (!invoiceData || invoiceData.length === 0) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <Empty text="No Customer found" textClassName="mt-2" />
+      </div>
+    );
+  }
 
   return (
     <TableLayout
